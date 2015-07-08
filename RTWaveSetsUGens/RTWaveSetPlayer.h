bb711ccb51a7030b119adf5a3f75b681d258ebc8
Fabian Seidl <fabian.seidl@me.com>
2015-07-08 17:05:40 +0200

    Some renamings.

--- RTWaveSetsUGens/RTWaveSetPlayer.h
+++ RTWaveSetsUGens/RTWaveSetPlayer.h
@@ -2,7 +2,7 @@
 #define RTWAVESETPLAYER_H
 
 #include "RTWaveSetBase.h"
-#include "waveset.h"
+#include "WaveSetPlayer.h"
 
 static const int minWavesetLength = (int) (3.0 * 44.100); // 3 ms
 static const int maxWavesetLength = (int) (500.0 * 44.100); // 500 ms
